4cc3c7496fb89befb3dbaefa0c20df84eb6aee19
Veaceslav Solotchi <veaceslav.solotchi@4finance.com>
2017-01-18 18:27:14 +0100

    gitignore non related project files

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -21,7 +21,7 @@ class Layout extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="wrapper">
         <Header />
         {this.props.children}
         <Feedback />
